ec04c7b9d95d75a23203ad7d94f75f16d4ebb69a
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-23 23:44:59 +0200

    Task 1 - Install a package with puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
-#!/usr/bin/pup
-# We use shebang because script will be executed
 # This file install flask from pip3 with Puppet
 # With a specific version of flask
 
 package { 'flask':
     ensure   => '2.1.0',
-    provider => 'pip3',
+    provider => 'pip3'
 }
