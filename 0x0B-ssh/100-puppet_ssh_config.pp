5e8985209b51212a36bf5216239a47db8629a6f4
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-26 01:43:56 +0200

    Advanced - Change conf file with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 # This script makes changes to our configuration file with puppet.
-imoprt stdlib
+include stdlib
 
 file_line { 'Turn off passwd auth': # Title of puppet resource (uniqe)
-    ensure  => present,
+    ensure  => 'present',
     path    => '/etc/ssh/ssh_config', # Path of file will be modefied
-    line    => '    PasswordAuthentication no', # Line if exists in file
+    line    => 'PasswordAuthentication no', # Line if exists in file
     replace => true # If PasswordAuthentication yes replace it with no
 }
 
 file_line { 'Delare identity file': # Title of puppet resource (uniqe)
-    ensure  => 'resent,
+    ensure  => 'present',
     path    => '/etc/ssh/ssh_config',
-    line    => '    IdentityFile ~/.ssh/school',
+    line    => 'IdentityFile ~/.ssh/school',
     replace => true
 }
