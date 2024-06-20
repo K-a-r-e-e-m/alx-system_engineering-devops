6b2c58091e298b4a29a37255d815a39b15ad5b08
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-06-20 18:10:08 +0300

    Task 0 - Get number of subscribers in subreddit from api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,14 @@ def number_of_subscribers(subreddit):
     """Return number of subscribers including not active users
     or zero at not valid subreddit"""
 
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = "https://www.reddit.com/"
-    myHeaders = {'User-Agent': 'Mozilla/5.0'}
+    myHeaders = {'User-Agent': 'script by u/KareeeemHany'}
     res = get(url + 'r/' + subreddit + '/about.json', headers=myHeaders)
 
-    if res.status_code == 200:
+    try:
         return res.json().get('data').get('subscribers')
-    else:
+    except Exception:
         return 0
