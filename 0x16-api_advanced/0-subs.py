20032e7492d2e6fca85498115e5ed8ae66ac2cf6
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-06-20 17:53:59 +0300

    Task 0 - Get number of subscribers in subreddit from api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
+"""This module have a function that quaries the Reddit API
+and returns number of subscribers for a given subreddit"""
+
 
 from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+    """Return number of subscribers including not active users
+    or zero at not valid subreddit"""
+    url = "https://www.reddit.com/"
+    # myHeaders = {'User-agent': 'script by u/KareeeemHany'}
+    res = get(url + 'r/' + subreddit + '/about.json')  # , headers=myHeaders)
+    if res.status_code == 200:
+        return res.json().get('data').get('subscribers')
+    else:
         return 0
