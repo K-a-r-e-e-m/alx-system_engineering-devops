a7afee6463a140345b50ef826ec28786c19959ed
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-06-21 17:08:54 +0300

    Task 1 - Get first 10 hot posts by api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
+"""This module have a function that queries the Reddit API and
+prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
 
-"""
-prints the titles of the first 10 hot posts listed for a given subreddit
-"""
 
 from requests import get
 
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
-    """
+    """Return first 10 hot post or zero at not valid subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    myHeaders = {'User-Agent': 'script by u/KareeeemHany'}
+    res = get(url, headers=myHeaders)
 
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
-    try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
+    if res.status_code == 200:
+        res = res.json()
+        for title in res['data']['children']:
+            print(title['data']['title'])
+    else:
         print("None")
